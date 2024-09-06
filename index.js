2a4ccb45643db4fadda599ac3364c1bc472c194b
Claudia Escarcega <l20170702@culiacan.tecnm.mx>
2024-09-06 16:38:05 -0700

    avance

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/inicio", (req, res) => {
+  res.render("inicio.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
